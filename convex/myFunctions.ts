7f2f539e8265d04f96addeed8d0560bbdc0d802f
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-07-09 00:19:10 +0530

    Fix typescript error

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { query, mutation, internalQuery, internalMutation } from "./_generated/server";
+import { query, mutation, internalQuery } from "./_generated/server";
 import { api } from "./_generated/api";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
