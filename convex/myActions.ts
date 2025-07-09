a083310f4061cf4a830730a8be8a8d08e1b4a64c
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-07-09 10:46:40 +0530

    Remove extra code + license

--- convex/myActions.ts
+++ convex/myActions.ts
@@ -120,32 +120,3 @@ export const newRound = action({
       }
     }
   });
-  
-  // You can fetch data from and send data to third-party APIs via an action:
-  export const myAction = action({
-    // Validators for arguments.
-    args: {
-      first: v.number(),
-      second: v.string(),
-    },
-  
-    // Action implementation.
-    handler: async (ctx, args) => {
-      //// Use the browser-like `fetch` API to send HTTP requests.
-      //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
-      // const response = await ctx.fetch("https://api.thirdpartyservice.com");
-      // const data = await response.json();
-  
-      //// Query data by running Convex queries.
-      const data = await ctx.runQuery(api.myFunctions.listNumbers, {
-        count: 10,
-      });
-      console.log(data);
-  
-      //// Write data by running Convex mutations.
-      await ctx.runMutation(api.myFunctions.addNumber, {
-        value: args.first,
-      });
-    },
-  });
-  
--- convex/schema.ts
+++ convex/schema.ts
@@ -7,9 +7,6 @@ import { authTables } from "@convex-dev/auth/server";
 // The schema provides more precise TypeScript types.
 export default defineSchema({
   ...authTables,
-  numbers: defineTable({
-    value: v.number(),
-  }),
   games: defineTable({
     createdBy: v.id("users"),
     theme: v.string(),
